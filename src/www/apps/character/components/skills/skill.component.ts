5ceefdc2770a888a81137821cc819b6b898854fb
Eric Thomas <eric.j.thomas1985@gmail.com>
2016-09-06 19:54:53 -0600

    Implementing Skills component.

---
+++ src/www/apps/character/components/skills/skill.component.ts
@@ -0,0 +1,57 @@
+namespace app.Character.Skills {
+	import ICharacter = app.character.model.ICharacter;
+
+	interface SkillScore {
+		Ability: string;
+		Ranks: number;
+		AbilityModifier: number;
+		ClassModifier: number;
+		MiscModifier: number;
+		TemporaryModifier: number;
+		ArmorClassPenalty: number;
+		Total: number;
+		Skill: ISkill;
+	}
+
+	interface ISkill {
+		Name: string;
+		AbilityType: string;
+		TrainedOnly: boolean;
+		ArmorCheckPenalty: boolean;
+	}
+
+	export class SkillController {
+		skill: SkillScore;
+
+		constructor() {
+		}
+
+		$onInit() {
+			console.log(this);
+		}
+
+		get Ability(): string {
+			return this.skill.Ability.substring(0, 3);
+		}
+	}
+
+	export class SkillComponent implements ng.IComponentOptions {
+		public controller: any;
+		public controllerAs: string = "ctrl";
+		public templateUrl: string;
+		public transclude: boolean = true;
+		public bindings: { [binding: string]: string };
+
+		constructor() {
+			this.controller = SkillController;
+			this.templateUrl = "/apps/character/components/skills/skill.tmpl.html";
+
+			this.bindings = {
+				skill: "<"
+			};
+		}
+	}
+
+	angular.module("character")
+		.component("skill", new SkillComponent());
+}
---
+++ src/www/apps/character/components/skills/skills.component.ts
@@ -0,0 +1,35 @@
+namespace app.Character.Skills {
+	import ICharacter = app.character.model.ICharacter;
+
+	export class SkillsController extends app.Character.BaseCardController {
+		character: ICharacter;
+
+		constructor() {
+			super();
+		}
+
+		$onInit() {
+			console.log(this);
+		}
+	}
+
+	export class SkillsComponent implements ng.IComponentOptions {
+		public controller: any;
+		public controllerAs: string = "cardController";
+		public templateUrl: string;
+		public transclude: boolean = true;
+		public bindings: { [binding: string]: string };
+
+		constructor() {
+			this.controller = SkillsController;
+			this.templateUrl = "/apps/character/components/skills/skills.tmpl.html";
+
+			this.bindings = {
+				character: "<"
+			};
+		}
+	}
+
+	angular.module("character")
+		.component("skills", new SkillsComponent());
+}
