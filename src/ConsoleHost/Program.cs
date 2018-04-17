ff0b5d3d3a96e9ea8ab830f95b71eb991f254b83
Eric Thomas <eric.j.thomas1985@gmail.com>
2018-04-16 22:18:44 -0600

    Adding ConsoleHost project for hosting WebApi outside of IIS

---
+++ src/ConsoleHost/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Web.Http.SelfHost;
+using Pathfinder.Api;
+using Pathfinder.Utilities;
+
+namespace ConsoleHost
+{
+	class Program
+	{
+		static readonly Uri _baseAddress = new Uri("http://localhost:8888/");
+		static void Main(string[] args)
+		{
+			LogTo.Info($"Launching WebApi Service host.");
+
+			AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
+
+			LogTo.Debug($"Configuring WebApi Services.");
+			// Set up server configuration
+			var config = new HttpSelfHostConfiguration(_baseAddress);
+			WebApiConfig.Register(config);
+
+			LogTo.Debug($"Bringing server online.");
+			var server = new HttpSelfHostServer(config);
+			// Start listening
+			server.OpenAsync().Wait();
+
+
+			LogTo.Info($"Web API Self hosted on {_baseAddress}");
+			PrintBanner();
+			var stop = false;
+			do
+			{
+				var input = Console.ReadLine();
+				switch (input)
+				{
+					case "s":
+					case "S":
+						stop = true;
+						break;
+					case "c":
+					case "C":
+						Console.Clear();
+						break;
+					case "?":
+						PrintBanner();
+						break;
+					default:
+						break;
+				}
+
+			} while (!stop);
+
+			server.CloseAsync().Wait();
+		}
+
+		private static void UnhandledExceptionTrapper(object pSender, UnhandledExceptionEventArgs pUnhandledExceptionEventArgs)
+		{
+			LogTo.Exception((Exception) pUnhandledExceptionEventArgs.ExceptionObject); ;
+
+			Console.WriteLine("Press Enter to continue");
+			Console.ReadLine();
+			Environment.Exit(1);
+		}
+
+		private static void PrintBanner()
+		{
+			Console.WriteLine(new string('=', 60));
+			Console.WriteLine($"Enter command:");
+			Console.WriteLine($"\t[sS]: Stop service");
+			Console.WriteLine($"\t[cC]: Clear console");
+			Console.WriteLine(new string('=', 60));
+		}
+	}
+}
